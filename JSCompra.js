2bb787846815188b3bf12a21fadae8a8c9423e6e
mauromantero <mauromantero@alumnat.iesdamiahuget.net>
2025-11-07 11:06:39 +0100

    Mejora de tienda

--- JSCompra.js
+++ JSCompra.js
@@ -1,18 +1,21 @@
-const compra1 = parseInt(document.getElementById('compra1').value);
-const compra2 = document.getElementById('compra2');
-const compra3 = document.getElementById('compra3');
-const compra4 = document.getElementById('compra4');
-const compra5 = document.getElementById('compra5');
-const compra6 = document.getElementById('compra6');
-const compra7 = document.getElementById('compra7');
-const compra8 = document.getElementById('compra8');
+let total= 0.00;
 
-
-function ColocarCompra () {
-    document.getElementById('Lista').style.display = 'block';
-    document.getElementById("Lista").innerText = compra1 + "€";
-}
-
-function ColocarX () {
+function ColocarCompra (nom, preu) {
     document.getElementById('BotonX').style.display = 'block';
+    document.getElementById('Lista').style.display = 'block';
+    document.getElementById("Lista").innerText += nom + ' - ' + preu + "€" + "\n"
+    let precio = parseFloat(preu);
+    total= precio + total
+    total.toFixed(2);
+    document.getElementById('Sumatotal').innerText = 'Total Aprox: ' + total;
+
 }
+
+function botoncerrar(){
+    document.getElementById('BotonX').style.display = 'none';
+    document.getElementById('Lista').style.display = 'none';
+    document.getElementById('Sumatotal').style.display = 'none';
+    document.getElementById("Lista").innerText = ""
+}
+
+
