4719e030178767a5a809ed542d99859a4c5dca27
mauromantero <mauromantero@alumnat.iesdamiahuget.net>
2025-11-11 11:04:25 +0100

    Tienda acabada

--- JSCompra.js
+++ JSCompra.js
@@ -1,13 +1,14 @@
 let total= 0.00;
 
-function ColocarCompra (nom, preu) {
+function ColocarCompra (nom, preu, imatge) {
     document.getElementById('BotonX').style.display = 'block';
     document.getElementById('Lista').style.display = 'block';
-    document.getElementById("Lista").innerText += nom + ' - ' + preu + "€" + "\n"
+    document.getElementById('Sumatotal').style.display = 'block';
+    document.getElementById('Lista').innerHTML+= "<div><img class='fotocompra' src=\""+imatge+"\" width=\"90\" height=\"90\">" + nom + "-" + preu + "€</div>";
     let precio = parseFloat(preu);
     total= precio + total
     total.toFixed(2);
-    document.getElementById('Sumatotal').innerText = 'Total Aprox: ' + total;
+    document.getElementById('Sumatotal').innerText = 'Total Aprox: ' + total.toFixed(2);
 
 }
 
@@ -16,6 +17,11 @@ function botoncerrar(){
     document.getElementById('Lista').style.display = 'none';
     document.getElementById('Sumatotal').style.display = 'none';
     document.getElementById("Lista").innerText = ""
+    total= 0.00;
 }
 
+// function xxxxx(){
+// (() = Al cargar la pagina / document.addEventListener("DOMContentLoaded", () => {
+//
 
+// localStorage.setitem("loquequierasguardar", let de lo q quieras guardar) + letdeloqueuguardes = localStorage.getitem(loquehasguardado)
