84f498f7e836fc1cb6322f2d37fbf08653706bb6
mauromantero <mauromantero@alumnat.iesdamiahuget.net>
2025-11-12 09:26:10 +0100

    Retoques finales

--- JSCompra.js
+++ JSCompra.js
@@ -1,4 +1,5 @@
 let total= 0.00;
+let productos= 0;
 
 function ColocarCompra (nom, preu, imatge) {
     document.getElementById('BotonX').style.display = 'block';
@@ -7,9 +8,10 @@ function ColocarCompra (nom, preu, imatge) {
     document.getElementById('Lista').innerHTML+= "<div><img class='fotocompra' src=\""+imatge+"\" width=\"90\" height=\"90\">" + nom + "-" + preu + "€</div>";
     let precio = parseFloat(preu);
     total= precio + total
+    productos = productos + 1;
     total.toFixed(2);
     document.getElementById('Sumatotal').innerText = 'Total Aprox: ' + total.toFixed(2);
-
+document.getElementById('Totalproductes').innerText = "Productos:  " + productos;
 }
 
 function botoncerrar(){
@@ -17,8 +19,11 @@ function botoncerrar(){
     document.getElementById('Lista').style.display = 'none';
     document.getElementById('Sumatotal').style.display = 'none';
     document.getElementById("Lista").innerText = ""
+    document.getElementById('Totalproductes').innerText = ""
+    productos = 0;
     total= 0.00;
 }
+
 
 // function xxxxx(){
 // (() = Al cargar la pagina / document.addEventListener("DOMContentLoaded", () => {
